6f15306fa99256cbec51e04d6261507ad3887320
Vikas Gupta <vgupta@st-d12178.local>
2014-12-09 14:12:59 +0100

    20141209

---
+++ java/.metadata/.plugins/org.eclipse.core.resources/.history/e1/90c34fc1326400141674f29106b61697
@@ -0,0 +1,5 @@
+public class variables {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ java/01_HelloWorld/src/conditional.java
@@ -0,0 +1,10 @@
+
+public class conditional {
+    public static void main(String[] args) {
+    	int a = 5;
+    	int result = a == 4 ? 1 : 8;
+    	// result will be 1
+    	// This is equivalent to
+    	System.out.println(result);
+    }
+}
